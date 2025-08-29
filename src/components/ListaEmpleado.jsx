32e0abb1a90da794d37de76d7ac9f1014e28d921
gurdian0614 <ivan.gurdian@outlook.com>
2025-08-28 20:42:20 -0600

    Build component

--- src/components/ListaEmpleado.jsx
+++ src/components/ListaEmpleado.jsx
@@ -2,7 +2,25 @@
 const ListaEmpleado = () => {
 
     return (
-        <h1>Lista</h1>
+        <div className="bg-white p-6 rounded-lg shadow-lg">
+            <h2 className="text-2xl font-semibold mb-4 text-gray-700">Lista de empleados</h2>
+
+            <div className="overflow-x-auto">
+                <table className="min-w-full bg-white border border-gray-200">
+                    <thead className="bg-gray-100">
+                        <tr>
+                            <th className="py-2 px-4 border-b">Nombre</th>
+                            <th className="py-2 px-4 border-b">Cargo</th>
+                            <th className="py-2 px-4 border-b">Departamento</th>
+                            <th className="py-2 px-4 border-b">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
     )
 }
 
